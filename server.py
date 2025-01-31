79cc8ebbd8a51bb692a7005dbde6ca18d9420fd1
ryan.silva <ryan.silva@sipam.gov.br>
2025-01-31 10:48:23 -0400

    manipulação com GET

--- server.py
+++ server.py
@@ -1,2 +1,19 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    # Manipula requisições GET
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(f"Hello, World! Esta e a página inicial.")
+        elif self.path == "/about":
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(f"Esta é a página Sobre.")
+        else:
+            self.send_error(404, "Página não encontrada.")
+            
+            
