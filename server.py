ba3f8f36b1caf9835b2393d1ca3259411b8f18d2
ryan.silva <ryan.silva@sipam.gov.br>
2025-01-31 13:45:49 -0400

    correções

--- server.py
+++ server.py
@@ -1,39 +1,50 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import signal
+import sys
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     # Manipula requisições GET
     def do_GET(self):
         if self.path == "/":
             self.send_response(200)
-            self.send_header('Content-type', 'text/html')
+            self.send_header('Content-type', 'text/html; charset=utf-8')
             self.end_headers()
-            self.wfile.write(f"Hello, World! Esta e a página inicial.")
+            self.wfile.write("Hello, World! Esta é a página inicial.".encode('utf-8'))
+            
         elif self.path == "/about":
             self.send_response(200)
-            self.send_header('Content-type', 'text/html')
+            self.send_header('Content-type', 'text/html; charset=utf-8')
             self.end_headers()
-            self.wfile.write(f"Esta é a página Sobre.")
+            self.wfile.write("Esta é a página Sobre.".encode('utf-8'))
+            
         else:
             self.send_error(404, "Página não encontrada.")
             
             
     # POST  
     def do_POST(self):
-        content_lenght = int(self.headers['Content-Lenght'])
-        post_data = self.rfile.read(content_lenght)
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-type', 'text/html; charset=utf-8')
         self.end_headers()
-        self.wfile.write(f"Você enviou: {post_data.decode('utf-8')}".enconde())
+        response_message = f"Você enviou {post_data.decode('utf-8')}"
+        self.wfile.write(response_message.encode('utf-8'))
         
         
         
-def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8080):
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8201):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print(f"Servidor rodando na porta {port}. . .")
+    
+    def signal_handler(sig, frame):
+        print("\nServidor sendo interrompido...")
+        httpd.server_close()
+        sys.exit(0)
+    
+    signal.signal(signal.SIGINT, signal_handler)
+    print(f"Servidor rodando na porta {port} . . .")
     httpd.serve_forever()
-        
         
 if __name__ == '__main__':
     run()
